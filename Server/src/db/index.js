781cf3e27fd790b986d2781ba3c4c61907d3c295
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-07-01 13:44:54 +0530

    refactor: update the db connection function

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    console.log('MONGO_URI:', process.env.MONGO_URI);
-console.log('DB_NAME:', process.env.DB_NAME);
-
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGO_URI}/${process.env.DB_NAME}`
