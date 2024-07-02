2780dc96833e7f34997b849b8e53b28cc22b97db
MUHAMMED FAYAZ T S <139971477+MuhammedFayazTS@users.noreply.github.com>
2024-07-02 15:23:24 +0530

    chore: add logger (#4)

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 
 const app = express();
 
@@ -14,6 +16,24 @@ app.use(
 app.use(express.json({ limit: "16Kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16Kb" }));
 app.use(express.static("public"));
+
+//logger
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 //import routes
 import healthcheckRouter from "./Routes/v1/healthcheck.routes.js";
