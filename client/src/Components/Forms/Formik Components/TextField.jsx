26d57baf54b267302ec688ab0adf80d77bb02224
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-07-15 17:08:06 +0530

    refactor: modify sign up form

--- client/src/Components/Forms/Formik Components/TextField.jsx
+++ client/src/Components/Forms/Formik Components/TextField.jsx
@@ -1,53 +1,47 @@
-import { useField } from 'formik'
-import React from 'react'
+import { useField } from 'formik';
+import React from 'react';
 import {
-    FormControl,
-    FormLabel,
-    FormHelperText,
-    FormErrorMessage,
-    Input,
-    InputGroup,
-    Button,
-    InputRightElement,
-    IconButton,
-} from '@chakra-ui/react'
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+} from '@chakra-ui/react';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
+function TextField({ label, noLabel, show, setShow, showProtectedText, placeholder, ...props }) {
+  const [field, meta] = useField(props);
 
-function TextField({ label, ...props }) {
+  const handleClick = () => setShow(!show);
 
-    const [field, meta] = useField(props) //input field and its meta data
-    // const [show, setShow] = React.useState(false)
-    const handleClick = () => props.setShow(!props.show)
-    return (
-        <div>
-            <FormControl isInvalid={meta.error && meta.touched}>
-                {!props.noLabel && <FormLabel>{label}</FormLabel>}
-                <InputGroup size='lg'>
-                    <Input variant='filled' size='lg' {...field} {...props}
-                        value={field.value || ''} onChange={field.onChange} placeholder={label} />
-                    <InputRightElement width='4.5rem'>
-                        {
-                            (field.name === 'password' || field.name === 'confirmPassword') && (
-                                <IconButton size='sm' isRound onClick={handleClick}>
-                                    {props.show ? <EyeIcon class="h-6 w-6" /> : <EyeSlashIcon class="h-6 w-6" />}
-                                </IconButton>
-                            )
-                        }
-
-                    </InputRightElement>
-                    {
-                        props.name === 'otp' &&
-                        <InputRightElement width='4.5rem'>
-                            <Button variant='ghost' size='sm' >send</Button>
-                        </InputRightElement>
-                    }
-                </InputGroup>
-                <FormErrorMessage>{meta.error}</FormErrorMessage>
-            </FormControl>
-        </div>
-    )
+  return (
+    <FormControl isInvalid={meta.touched && meta.error}>
+      {!noLabel && <FormLabel htmlFor={props.name}>{label}</FormLabel>}
+      <InputGroup size="lg">
+        <Input
+          variant="filled"
+          size="lg"
+          fontSize={'medium'}
+          {...field}
+          {...props}
+          value={field.value || ''}
+          onChange={field.onChange}
+          placeholder={placeholder}
+          type={showProtectedText && !show ? 'password' : 'text'}
+        />
+        {showProtectedText && (
+          <InputRightElement width="4.5rem">
+            <IconButton size="sm" isRound onClick={handleClick}>
+              {show ? <EyeIcon className="h-6 w-6" /> : <EyeSlashIcon className="h-6 w-6" />}
+            </IconButton>
+          </InputRightElement>
+        )}
+      </InputGroup>
+      <FormErrorMessage>{meta.error}</FormErrorMessage>
+    </FormControl>
+  );
 }
 
-export default TextField
+export default TextField;
